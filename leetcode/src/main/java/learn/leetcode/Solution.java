7f2b4cca7f64aef60e95a0a1c06c93b8b359980c
执笔 <805073259@qq.com>
2019-09-11 14:34:38 +0800

    乐扣第一题

---
+++ leetcode/src/main/java/learn/leetcode/Solution.java
@@ -0,0 +1,35 @@
+package learn.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
+ * @author 执笔
+ * @date 2019/9/11 13:44
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[]                 arr = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (map.containsKey(target - num)) {
+                arr[0] = i;
+                arr[1] = map.get(target - num);
+                break;
+            }
+            map.put( num, i);
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[]    ints     = solution.twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println(ints[0] + " " + ints[1]);
+    }
+}
